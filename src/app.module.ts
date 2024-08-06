1e2c5a8df154f277bfc6e2d8d40f814fa5ebbd5d
Sophie Santana <ssophiesantana@gmail.com>
2024-08-05 23:00:18 -0300

    feat: serve api e postgres no docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoListModule } from './todo-list/todo-list.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [TodoListModule],
+  imports: [TodoListModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: false,
+      entities: [`${__dirname}/src/**/*.entity.{ts,js}`],
+    }),
+  ],
+})
+export class DatabaseModule {}
